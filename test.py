64b08c1b08bc209346f97394b6adefb788e0dab9
bang <dpclub99@gmail.com>
2024-08-05 10:43:00 +0900

    출도착지 입력 구현

--- test.py
+++ test.py
@@ -12,6 +12,11 @@ def getUserAccount():
     pw = input("비밀번호를 입력하세요 : ")
     return id, pw
 
+def getStation():
+    dep = input("출발역을 입력하세요 : ")
+    arr = input("도착역을 입력하세요 : ")
+    return dep, arr
+
 # 웹드라이버 설정
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -26,5 +31,19 @@ driver.find_element(By.ID, 'hmpgPwdCphd01').send_keys(pw) # 비밀번호
 driver.find_element(By.XPATH, '//*[@id="login-form"]/fieldset/div[1]/div[1]/div[2]/div/div[2]/input').click()
 driver.implicitly_wait(5)
 
+# 예매 페이지 이동
 driver.get('https://etk.srail.kr/hpg/hra/01/selectScheduleList.do')
 driver.implicitly_wait(5)
+
+# 출발지, 도착지 입력
+dep, arr = getStation()
+dep_stn = driver.find_element(By.ID, 'dptRsStnCdNm')
+dep_stn.clear() 
+dep_stn.send_keys(dep)
+
+arr_stn = driver.find_element(By.ID, 'arvRsStnCdNm')
+arr_stn.clear()
+arr_stn.send_keys(arr)
+
+
+time.sleep(300) # 종료 방지
